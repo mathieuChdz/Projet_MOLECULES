e6fe7e142fc55ab326453191bdc3a34f995a921a
sinaypan <yanisarris11@gmail.com>
2026-02-10 22:24:47 +0100

    Implement ShapeTanimoto function for molecular shape similarity calculation

--- Distance.py
+++ Distance.py
@@ -14,7 +14,20 @@ def Tanimoto2D(m1, m2):
     return DataStructs.TanimotoSimilarity(fp1, fp2)
 
 def ShapeTanimoto(m1, m2):
+    mol1 = Chem.MolFromMolFile(m1)
+    mol2 = Chem.MolFromMolFile(m2)
+
+    if mol1 is None or mol2 is None: return 0.0
     return 0.0
+
+    m1 = Chem.AddHs(mol1)
+    m2 = Chem.AddHs(mol2)  
+
+    allChem = AllChem.EmbedMolecule(m1)
+    allChem = AllChem.EmbedMolecule(m2)
+
+    return AllChem.ShapeTanimotoDist(m1, m2)
+
 
 def score(m1, m2, a):
     return  a * Tanimoto2D(m1, m2) + (1-a) * ShapeTanimoto(m1, m2)
