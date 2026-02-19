ec0db24ce1ca68335f9fff61589b8203d968f285
HAMOUDI nabil <nabil.hamoudi@ens.uvsq.fr>
2026-02-19 02:31:25 +0100

    Jaout de la template et fix isomorphisme

--- Check_iso.c
+++ Check_iso.c
@@ -1,10 +1,18 @@
 #include "nauty.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_signature(char* path) {
     FILE *f = fopen(path, "r");
-    int n, e;
-    if (fscanf(f, "%d %d", &n, &e) != 2) return;
+    if (!f) return;
+
+    int n;
+    if (fscanf(f, "%d", &n) != 1) {
+        fclose(f);
+        return;
+    }
+
+    int m = SETWORDSNEEDED(n);
 
     DYNALLSTAT(graph, g, g_sz);
     DYNALLSTAT(int, lab, lab_sz);
@@ -16,7 +24,7 @@ void print_signature(char* path) {
     DYNALLOC1(int, lab, lab_sz, n, "malloc");
     DYNALLOC1(int, ptn, ptn_sz, n, "malloc");
     DYNALLOC1(int, orbits, orbits_sz, n, "malloc");
-    DYNALLOC2(graph, g, g_sz, n, SETWORDSNEEDED(n), "malloc");
+    DYNALLOC2(graph, g, g_sz, n, m, "malloc");
 
     int *colors = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
@@ -24,44 +32,55 @@ void print_signature(char* path) {
     }
 
     int current_lab = 0;
-    int color_list[200];
-    int color_count = 0;
-
     for (int c = 0; c < 255; c++) {
-        int found = 0;
+        int count_in_color = 0;
+        
         for (int i = 0; i < n; i++) {
             if (colors[i] == c) {
                 lab[current_lab] = i;
-                ptn[current_lab] = 1;
+                ptn[current_lab] = 0;
                 current_lab++;
-                found = 1;
+                count_in_color++;
             }
         }
-        if (found) ptn[current_lab - 1] = 0; // Fin d'un groupe de même couleur
-    }
-    free(colors);
+        
 
-    options.getcanon = 1;
-    options.defaultptn = FALSE;
-    EMPTYGRAPH(g, SETWORDSNEEDED(n), n);
-
-    int u, v;
-    for (int i = 0; i < e; i++) {
-        if (fscanf(f, "%d %d", &u, &v) == 2) {
-            ADDONEEDGE(g, u, v, SETWORDSNEEDED(n));
+        if (count_in_color > 0) {
+            ptn[current_lab - 1] = 1; 
         }
+    }
+
+    // --- 3. Construction du Graphe ---
+    EMPTYGRAPH(g, m, n);
+    int u, v;
+    while (fscanf(f, "%d %d", &u, &v) == 2) {
+        ADDONEEDGE(g, u, v, m);
+        ADDONEEDGE(g, v, u, m);
     }
     fclose(f);
 
-    DYNALLSTAT(graph, canong, canong_sz);
-    DYNALLOC2(graph, canong, canong_sz, n, SETWORDSNEEDED(n), "malloc");
-    densenauty(g, lab, ptn, orbits, &options, &stats, SETWORDSNEEDED(n), n, canong);
 
-    for (int i = 0; i < SETWORDSNEEDED(n) * n; i++) {
+    options.getcanon = 1;
+    options.defaultptn = FALSE;
+
+    DYNALLSTAT(graph, canong, canong_sz);
+    DYNALLOC2(graph, canong, canong_sz, n, m, "malloc");
+
+
+    densenauty(g, lab, ptn, orbits, &options, &stats, m, n, canong);
+
+
+    for (int i = 0; i < n; i++) {
+        printf("%d_", colors[lab[i]]);
+    }
+    printf("|");
+
+    for (int i = 0; i < m * n; i++) {
         printf("%lx", canong[i]);
     }
     printf("\n");
 
+    free(colors);
     DYNFREE(g, g_sz); DYNFREE(canong, canong_sz);
     DYNFREE(lab, lab_sz); DYNFREE(ptn, ptn_sz); DYNFREE(orbits, orbits_sz);
 }
